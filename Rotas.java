28aa8fac17b043ed12bcfc8e352055c98a3ac5d4
Igor Ricci Constantino <99025027+igorconstantino@users.noreply.github.com>
2022-05-27 09:56:55 -0300

    commit Rotas.java

---
+++ Rotas.java
@@ -0,0 +1,113 @@
+public class Cidades
+{
+    private String origem;
+    private String parada;
+    private String destino;
+    private Date saida;
+    private Date chegada;
+    private Onibus bus;
+    private double valor;
+    
+    // Construtores
+
+    public Cidades(String o, String p, String d, int sDia, int sMes, int sAno, int sHora, int sMin, int cDia, int cMes, int cAno, int cHora, int cMin, Onibus bus, double valor)
+    {
+        this.origem = o;
+        
+        if(o != p)
+            this.parada = p;
+
+        if(d != p && o != d)
+           this.destino = d;
+
+        this.saida = new Date(sDia, sMes, sAno, sHora, sMin);
+
+        this.chegada = new Date(cDia, cMes, cAno, cHora, cMin);
+
+        this.bus = bus;
+
+        if(valor > 0)
+            this.valor = valor;
+    }
+    
+    public Cidades()
+    {
+        this("","","", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null, 0.0);
+    }
+    
+    // Setters
+    
+    public void setOrigem(String o)
+    {
+        this.origem = o;
+    }
+    
+    public void setParada(String p)
+    {
+        this.parada = p;
+    }
+    
+    public void setDestino(String d)
+    {
+        this.destino = d;
+    }
+
+    public void setSaida(int sDia, int sMes, int sAno, int sHora)
+    {
+        this.saida = new Date(sDia, sMes, sAno, sHora);
+    }
+
+    public void setChegada(int cDia, int cMes, int cAno, int cHora)
+    {
+        this.chegada = new Date(cDia, cMes, cAno, cHora);
+    }
+
+    public void setOnibus(Onibus bus)
+    {
+        this.bus = bus;
+    }
+
+    public void setValor(double valor)
+    {
+        if(valor > 0)
+            this.valor = valor;
+    }
+
+    // Getters
+    
+    public String getOrigem()
+    {
+        return this.origem;
+    }
+    
+    public String getParada()
+    {
+        return this.parada;
+    }
+    
+    public String getDestino()
+    {
+        return this.destino;
+    }
+
+    public Date getSaida()
+    {
+        return this.saida;
+    }
+
+    public Date getChegada()
+    {
+        return this.chegada;
+    }
+
+    public Onibus getBus()
+    {
+        return this.bus;
+    }
+
+    public double getValor()
+    {
+        return this.valor;
+    }
+
+}
